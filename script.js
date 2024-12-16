1e092acbfabddd0fb9b7c9348ab95c2626c57d97
idobenel <88383975+idobenel@users.noreply.github.com>
2024-12-16 10:54:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -23,10 +23,13 @@ function searchGuest() {
     const resultElement = document.getElementById("result");
     const tableNumber = document.getElementById("tableNumber");
     const guestCount = document.getElementById("guestCount");
+    const db_name = document.getElementById("db_name");
 
     // Reset result
+    db_name.textContent = "שם: ";
     tableNumber.textContent = "מספר שולחן: ";
     guestCount.textContent = "מספר אורחים: ";
+    
 
     if (name === "") {
         alert("הכנס את שם האורח");
@@ -34,12 +37,14 @@ function searchGuest() {
     }
 
     // Search for the guest
-    const guest = guests.find(g => g.name.toLowerCase() === name.toLowerCase());
+    const guest = guests.find(g => g.name.toLowerCase().includes(name.toLowerCase()));
 
     if (guest) {
+        db_name.textContent = "שם: "; + guest.name;
         tableNumber.textContent = "מספר שולחן: " + guest.table_number;
         guestCount.textContent = "מספר אורחים: " + guest.number_of_guests;
     } else {
+    	db_name.textContent = "שם לא נמצא";
         tableNumber.textContent = "לא נמצא מספר שולחן";
         guestCount.textContent = "לא נמצאו מספר אורחים";
     }
